fd3f062a488dd24d1b3dd0dae734699c759ffc78
kj22594 <kstaley22594@gmail.com>
2016-12-06 20:25:44 -0500

    Updated stylesheet, and database in connection.js

--- connection.js
+++ connection.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
-    database: "finalProject"
+    database: "playerValueTool"
 });
 
 connection.connect(function(err) {
